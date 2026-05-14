b6e806ee48204d3c22b7d15bf4a59f27eb5feccc
Felipe Benavides <pipeb62@gmail.com>
2026-05-14 16:04:48 +0200

    Make computer play against itself. Check that vision graph is consistent

--- Play.jl
+++ Play.jl
@@ -7,7 +7,7 @@ function show_moves(moves)
     end
 end 
 
-function main()
+function pvc()
     playercolor = white
     computercolor = black
     game_state = initalize_board()
@@ -43,5 +43,44 @@ function main()
         display(game_state)
     end
 
+end
+
+function cvc()
+
+    game_state = initalize_board()
+    display(game_state)
+    println()
+    
+    for i in 1:100
+        allmoves = get_all_moves(game_state)
+        if length(allmoves)==0
+            if game_state.check
+                print("checkmate")
+            else
+                print("stalemate")
+            end
+            break
+        end
+        
+        #show_moves(allmoves)
+        moveindex = rand(1:length(allmoves))  
+
+        move = allmoves[moveindex]
+        readline()
+        println("\033c")
+        println(game_state.turn, " move: ", move)
+
+        make_move!(game_state,move)
+        display(game_state)
+
+        #consistensy check
+        vision_graph_check = create_vision_graph(game_state.board, game_state.pieces)
+        println("Vision graph consistent: ",vision_graph_check==game_state.vision_graph)
+    end
+
+end
+
+function main()
+    cvc()
 end
 main()
